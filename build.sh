a56471360cab227f68c39f6ef8a6b6393ed4393e
ihipop <ihipop@gmail.com>
2022-07-20 12:34:35 +0800

    update build script

--- build.sh
+++ build.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
-sudo apt install gcc g++ -y
-mkdir -p target
-make clean
-make ZT_STATIC=1 one && strip ./zerotier-one && mv  ./zerotier-one target/zerotier-one.`gcc -dumpmachine` && \
-for abi in aarch64-linux-gnu arm-linux-gnueabihf mips-linux-gnu mips64-linux-gnuabi64 mipsel-linux-gnu mips64el-linux-gnuabi64;do
-    sudo apt install gcc-${abi} g++-${abi} -y && \
-    CC=${abi}-gcc && \
-    CXX=${abi}-g++ && \
-    STRIP=${abi}-strip && \
-    make clean && \
-    make ZT_STATIC=1 CC=${CC} CXX=${CXX} one && \
-    $STRIP ./zerotier-one && \
-    file ./zerotier-one && \
-    mv ./zerotier-one ./target/zerotier-one.${abi}
+apt -qq update && apt -qq install cargo build-essential git pkg-config libssl-dev -y
+set -e
+apt install gcc g++ -y
+TARGET_DIR=${TARGET_DIR:-"./target"}
+TARGET_DIR=$(readlink -f "${TARGET_DIR}")
+mkdir -p "${TARGET_DIR}"
+echo -n > info.txt
+for abi in x86_64-linux-gnu i686-linux-gnu aarch64-linux-gnu arm-linux-gnueabihf mips-linux-gnu mips64-linux-gnuabi64 mipsel-linux-gnu mips64el-linux-gnuabi64;do
+    command -v ${abi}-gcc || apt install gcc-${abi} g++-${abi} -y
+    CC=${abi}-gcc
+    CXX=${abi}-g++
+    STRIP=${abi}-strip
+    make clean
+    make ZT_STATIC=1 CC=${CC} CXX=${CXX} ZT_SSO_SUPPORTED=0 one  || exit 1
+    $STRIP ./zerotier-one
+    tar cvf zerotier-one_${abi}_static.tar.gz zerotier-one
+    info=$(file zerotier-one)
+    echo "${info/#zerotier-one/zerotier-one_${abi}_static}" >> info.txt
+    mv *.gz "${TARGET_DIR}"
 done
-file ./target/zerotier-one*
+echo "###############################"
+cat info.txt
